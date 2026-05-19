c9e60259b8b6d76d0086383b78811cd1ef260df4
DeDPartners <cristiano@dedpartners.com>
2026-05-19 12:20:26 +0200

    [FIX] Rimosso header_hamburger non esistente in Odoo 19

--- theme_ded_partners/models/theme_ded_partners.py
+++ theme_ded_partners/models/theme_ded_partners.py
@@ -6,4 +6,3 @@ class ThemeUtils(models.AbstractModel):
 
     def _theme_ded_partners_post_copy(self, mod):
         self.enable_view('website.template_footer_contact')
-        self.enable_view('website.header_hamburger')
